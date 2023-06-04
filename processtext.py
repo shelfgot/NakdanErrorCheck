6a230bc6c4c69955abc561e525c3d2841f8b4688
Shlomi Helfgot <shlomihelfgot@gmail.com>
2023-06-04 19:49:20 -0400

    Intermediate stage of class refactor

--- processtext.py
+++ processtext.py
@@ -1,10 +1,19 @@
 import re
 
 def get_words(file_name):
+    """
+    Returns a List of space-separated words from a given file.
+    file_name: string
+    """
     words_file = open(file_name, 'r', encoding="utf8")
     text = words_file.read()
     words = text.split(" ")
+    return words
 def process_words(words):
+    """Returns a Dict of words, where the keys are the unvocalized word form (tzurah) and the values are a two member array. 
+    The first member of this array is a Dict where each form (tzurah) is a key, and the values are the locations of the tzurah. 
+    The second member of the array keeps track of how many words have been processed with the archetypal unvocalized form.
+    """
     word_dict = {}
     for index,word in enumerate(words):
         unvocalized_word = re.sub(r'[ְֱִֵֶַָֹֻּ]', '', word, flags=re.IGNORECASE)
@@ -32,7 +41,7 @@ def generate_suggestions(tzurah_family):
     ordered_tzurot = dict(sorted(tzurah_family[0].items(), key=lambda kv: len(kv[1]), reverse=True))
     most_common = next(iter(ordered_tzurot))
     ordered_tzurot.pop(most_common)
-    if((len(tzurah_family[0][most_common]) * 1.5) > tzurah_family[1]):
+    if (len(tzurah_family[0][most_common]) * 1.5) > tzurah_family[1]:
         suggestions = {'best_guess': most_common, 'fixes': ordered_tzurot}
         return suggestions
 
@@ -46,3 +55,6 @@ def compile_suggestions(corpus):
         if suspicious_tzurot is not None:
             suggestions.append(suspicious_tzurot)
     return {'words': corpus_words, 'suggestions': suggestions}
+
+if __name__ == "__main__":
+    print(compile_suggestions("words.txt"))
